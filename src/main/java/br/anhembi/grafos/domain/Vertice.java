08474a572b42feeb98cf496f5e1fd82da374c0b1
Luiz Felipe <luizfelipe.learning@gmail.com>
2022-11-18 08:31:10 -0300

    Alteração na classe Vertice

--- src/main/java/br/anhembi/grafos/domain/Vertice.java
+++ src/main/java/br/anhembi/grafos/domain/Vertice.java
@@ -1,56 +1,43 @@
 package br.anhembi.grafos.domain;
 
-import java.util.*;
-
 public class Vertice {
 
     private String nome;
-
-    private List<Vertice> caminhoMaisCurto = new LinkedList<>();
-
-    private Integer distancia = Integer.MAX_VALUE;
-
-    private Map<Vertice, Integer> nodesAdjacentes = new HashMap<>();
-
-    public void addDestino(Vertice destination, int distance) {
-        nodesAdjacentes.put(destination, distance);
-    }
 
     public Vertice(String nome) {
         this.nome = nome;
     }
 
-
-
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public String toString() {
+        return "(" + nome + ")";
     }
 
-    public List<Vertice> getCaminhoMaisCurto() {
-        return caminhoMaisCurto;
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
     }
 
-    public void setCaminhoMaisCurto(List<Vertice> caminhoMaisCurto) {
-        this.caminhoMaisCurto = caminhoMaisCurto;
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Vertice other = (Vertice) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
     }
 
-    public Integer getDistancia() {
-        return distancia;
-    }
 
-    public void setDistancia(Integer distancia) {
-        this.distancia = distancia;
-    }
-
-    public Map<Vertice, Integer> getNodesAdjacentes() {
-        return nodesAdjacentes;
-    }
-
-    public void setNodesAdjacentes(Map<Vertice, Integer> nodesAdjacentes) {
-        this.nodesAdjacentes = nodesAdjacentes;
-    }
 }
